11b8f9b96f445f8331235b9524777e742a4b601b
Reyan1234567 <reyann.14b@gmail.com>
2025-11-28 22:08:40 +0300

    initial commit

---
+++ Models/PizzaDb.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaStore.Models;
+
+public class PizzaDb(DbContextOptions<PizzaDb> options) : DbContext(options)
+{
+    public DbSet<Pizza> Pizzas { get; set; } = null!;
+}
